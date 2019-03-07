64d5ef699e8927829c809c4f16837d688f63fe9e
mmmatthew <matthew.mdv@gmail.com>
2019-03-07 13:48:46 +0100

    update requirements

--- example/iteration_example.py
+++ example/iteration_example.py
@@ -14,5 +14,5 @@ for i in [5000]:
     nowstring = datetime.strftime(datetime.now(), '%Y%m%d_%H.%M')
     temp_folder = 'results/versions/1.4.5' # + nowstring
     exp = experiment_runner.ExperimentRunner(data_directory=temp_folder, output_file='results/experiments.csv',
-                                             settings=s)
+                                             settings=s, experiment_metadata={'count_sensors':3})
     exp.run(print_final=True, repetitions=5000, kstop=5, ngs=5, pcento=0.5)
