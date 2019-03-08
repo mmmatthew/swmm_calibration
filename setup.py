91de39ee26f68fbdc71007a412d566a26b7bbcbf
mmmatthew <matthew.mdv@gmail.com>
2019-03-08 15:43:57 +0100

    fix setup info

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name='swmm_calibration',
+    name='swmmcalibration',
     version='0.1',
     author="Matthew Moy de Vitry",
     author_email="matthew.moydevitry@eawag.ch",
     description="A tool to degrade time series data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mmmatthew/timeseries-degradation",
+    url="https://github.com/mmmatthew/swmm_calibration",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
